418311a874f762c7202f599984e859be71689851
SuiLing237 <54572003+SuiLing237@users.noreply.github.com>
2020-03-10 17:09:58 +0800

    Create priceList.sql

---
+++ priceList.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 14, 2019 at 06:42 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: 'priceList'
+--
+CREATE DATABASE IF NOT EXISTS `patient` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `patient`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+DROP TABLE IF EXISTS `patient`;
+CREATE TABLE IF NOT EXISTS `patient` (
+  `patientID` int(64) NOT NULL,
+  `patient_name` varchar(64) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone` int(8) NOT NULL,
+--   `prescription` varchar(100),
+  PRIMARY KEY (`patientID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
