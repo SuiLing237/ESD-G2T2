274ad705ffd31158fd4b5d5a7b5f2bc7caa78813
Lee Mu Shi <mushi.lee.2018@sis.smu.edu.sg>
2020-03-31 16:53:52 +0800

    Prescription retrieve 1 medicineID and its quantity

--- prescription.py
+++ prescription.py
@@ -33,14 +33,74 @@ class Prescription(db.Model):
 def home():
     return "Your application is working!"
 
+# Json input format:
+# {
+#   "BookingId": "1",
+#   "Prescription": [
+#     {
+#       "med_id": "9781434474234",
+#       "quantity": 1
+#     },
+#     {
+#       "med": "9781449474212",
+#       "quantity": 1
+#     }
+#   ]
+# }
+
+@app.route("/prescription/", methods["POST"])
+def create_prescription(prescription):
+ # status in 2xx indicates success
+    status = 201
+    result = {}
+
+    # retrieve information about order and order items from the request
+    patient_id = request.json.get('patient_id', None)
+     = Order(patient_id = patient_id)
+    prescription = request.json.get('prescription')
+    for index, ci in enumerate(prescription):
+        pass
+    #     if 'med_id' in prescription[index] and 'quantity' in prescription[index]:
+    #         order.order_item.append(Order_Item(med_id = prescription[index]['med_id'], quantity = prescription[index]['quantity']))
+    #     else:
+    #         status = 400
+    #         result = {"status": status, "message": "Invalid 'med_id' or 'quantity'."}
+    #         break
+
+    # if status==201 and len(order.order_item)<1:
+    #     status = 404
+    #     result = {"status": status, "message": "Empty order."}
+
+    # if status==201:
+    #     try:
+    #         db.session.add(order)
+    #         db.session.commit()
+    #     except Exception as e:
+    #         status = 500
+    #         result = {"status": status, "message": "An error occurred when creating the order in DB.", "error": str(e)}
+        
+    #     if status==201:
+    #         result = order.json()
+
+    # # FIXME: add a call to "send_order" copied from another appropriate file
+    # send_order(result)
+    # return str(result), status
+    return
+
+@app.route("/<int:patientID>/<int:bookingID>")
+def retrieve_prescription(patientID,bookingID):
+    prescription = Prescription.query.filter_by(patientID=patientID).filter_by(bookingID= bookingID) #assume that each patient has only one prescription in databse
+    if prescription:
+        return jsonify(prescription.json())
+    return jsonify({"message": "Prescription does not exist."}), 404
 
 # This is a function to trigger send_prescriptionn
 @app.route("/prescription/send")
 def start_send_prescription():
-    messege = json.dumps(medicineID, medicine_quantity, default=str) # convert a JSON object to a string
+    message = json.dumps(medicineID, medicine_quantity, default=str) # convert a JSON object to a string
 
     #send to medicine microservice
-    r = requests.post(pricelistURL, json = messege)
+    r = requests.post(pricelistURL, json = message)
     print("Price sent to paypal.")
 
 # # AMQP Function for sending Prescription
