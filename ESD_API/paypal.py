955148a00ee124c880c8db0af0ccdd6e7e7f7a91
Lee Mu Shi <mushi.lee.2018@sis.smu.edu.sg>
2020-04-08 01:33:39 +0800

    **Payment db + Completed Payment

---
+++ ESD_API/paypal.py
@@ -0,0 +1,118 @@
+from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import paypalrestsdk
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/payment' # ENTER DB NAME HERE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+class Payment(db.Model):
+    __tablename__ = "payment"
+
+    patientID = db.Column(db.Integer, primary_key=True)
+    bookingID = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    
+    def __init__(self, patientID, bookingID, total_price):
+        self.patientID = patientID
+        self.bookingID = bookingID
+        self.total_price = total_price
+    
+    def json(self):
+        return {"patientID": self.patientID, "bookingID": self.bookingID, "total_price": self.total_price}
+
+
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "AT_uRRX_jQsZVKZXU-iMHj5qIcIfpc7aLh8kKgljsLcSJWcHmA030sBhcpwtI9LqVzszFX9aZJ0TjtVl",
+  "client_secret": "EP1fgGYD5F73c5IlleCA7nVg6ivbeIIwPVYDbUdT5wK1CUyJus6gtkGAsgyHVizifJL_ufsVVKpkz-3L" })
+
+@app.route('/')
+def index():
+    return render_template('billing.html')
+
+@app.route('/create_payment/<int:patientID>/<int:bookingID>', methods=['POST'])
+def create_payment(patientID, bookingID):
+    if (Payment.query.filter_by(patientID=patientID, bookingID=bookingID).first()): 
+        return jsonify({"message": "A record with patientID '{}' and bookingID '{}' already exists.".format(patientID, bookingID)}), 400
+
+    data = request.get_json() # <- POSTMAN. get request from POSTMAN
+    d = Payment(patientID, bookingID, **data) # **data is to get all the fields (e.g. availability, etc)
+    print(data)
+    print(d.json())
+
+    try:
+        db.session.add(d) # add the book to Database
+        db.session.commit() 
+    # except SQLAlchemyError as e:
+    #     error = str(e.__dict__['orig'];
+    #     return 'error'
+    #     # return jsonify({"message": "An error occurred creating the payment record."}), 500 # ERRROR MSG
+
+    except:
+        return jsonify({"message": "An error occurred creating the book."}), 500
+
+    return jsonify(d.json()), 201
+@app.route('/retrieve_price/<int:patientID>/<int:bookingID>')
+def retrieve_price(patientID, bookingID):
+    details= Payment.query.filter_by(patientID=patientID, bookingID=bookingID).first()
+    if details:
+        payment = details.json()
+        return payment
+        # return jsonify(book.json())
+    return jsonify({"message": "Book not found."}), 404
+@app.route('/payment/<int:patientID>/<int:bookingID>', methods =['POST'])
+def process_payment(patientID, bookingID):
+    details= Payment.query.filter_by(patientID=patientID, bookingID=bookingID).first()
+    if details:
+        payment_amount = details.total_price
+        print(payment_amount)
+        payment = paypalrestsdk.Payment({
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"},
+            "redirect_urls": {
+                "return_url": "http://localhost:3000/payment/execute",
+                "cancel_url": "http://localhost:3000/"},
+            "transactions": [{
+                "item_list": {
+                    "items": [{
+                        "name": "Medical Bill",
+                        "sku": "12345",
+                        "price": payment_amount,
+                        "currency": "SGD",
+                        "quantity": 1}]},
+                "amount": {
+                    "total": payment_amount,
+                    "currency": "SGD"},
+                "description": "Medical prescription bill."}]})
+
+        if payment.create():
+            print('Payment created!')
+        else:
+            print(payment.error)
+
+        return jsonify({'paymentID' : payment.id})
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    success = False
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id' : request.form['payerID']}):
+        print('Execute success!')
+        success = True
+    else:
+        print(payment.error)
+
+    return jsonify({'success' : success})
+
+if __name__ == '__main__':
+    app.run(port=5003, debug=True)
---
+++ sql/payment.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 14, 2019 at 06:42 AM
+-- Server version: 5.7.19
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: 'patient'
+--
+CREATE DATABASE IF NOT EXISTS `payment` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `payment`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE IF NOT EXISTS `payment` (
+  `patientID` int(64) NOT NULL,
+  `bookingID` int(64) NOT NULL,
+  `total_price` double(64,2) NOT NULL,
+  PRIMARY KEY (`bookingID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Add in `payment` values
+INSERT INTO `payment` (`patientID`, `bookingID`, `total_price`) VALUES
+(1, 1, 90.00);
+COMMIT;
